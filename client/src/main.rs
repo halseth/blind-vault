905003b1be5491fe64ff8c08c1b22c3a969c6400
Johan T. Halseth <johanth@gmail.com>
2025-04-01 15:11:40 +0200

    client: extract into methods

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,17 +1,22 @@
+use actix_web::middleware::Logger;
+use actix_web::{App, HttpServer, Responder, Result, get, post, web};
+use bitcoin::{Address, Network, TxOut};
 use clap::Parser;
 use hex::ToHex;
 use musig2::secp::{G, MaybePoint, MaybeScalar, Point, Scalar};
 use musig2::{
-    AggNonce, KeyAggContext, PartialSignature, PubNonce, compute_challenge_hash_tweak,
+    AggNonce, KeyAggContext, PartialSignature, PubNonce, SecNonce, compute_challenge_hash_tweak,
     verify_partial_challenge,
 };
 use rand::Rng;
 use secp256k1::{PublicKey, Secp256k1, SecretKey};
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
-use shared::{InitResp, SignReq, SignResp};
+use shared::{InitResp, SignPsbtReq, SignPsbtResp, SignReq, SignResp};
 use std::collections::HashMap;
+use std::net::SocketAddr;
 use std::str::FromStr;
+use std::sync::Mutex;
 
 #[derive(Debug, Parser)]
 #[command(verbatim_doc_comment)]
@@ -20,16 +25,11 @@ struct Args {
     cfg: Option<String>,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 struct Config {
     pub signers: Vec<String>,
 }
 
-struct SigningSession {
-    signer: String,
-    session_id: String,
-    init_resp: InitResp,
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -40,68 +40,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cfg: Config = serde_json::from_str(&args.cfg.unwrap()).unwrap();
     println!("config: {:?}", cfg);
 
-    let mut sessions = vec![];
+    run_example(cfg).await.unwrap();
+    return Ok(());
+}
 
-    for s in cfg.signers {
-        let id = hex::encode(rand::thread_rng().random::<[u8; 32]>());
-        let resp = reqwest::get(format!("http://{s}/init/{id}"))
-            .await?
-            .json::<InitResp>()
-            .await?;
-        println!("{resp:#?}");
+async fn run_example(cfg: Config) -> Result<(), Box<dyn std::error::Error>> {
+    let secp = Secp256k1::new();
+    let message = "hello interwebz!";
 
-        let session = SigningSession {
-            signer: s,
-            session_id: id.clone(),
-            init_resp: resp.clone(),
-        };
-
-        sessions.push(session);
-    }
-
+    let sessions = init_signer_sessions(&cfg).await?;
     let num_signers = sessions.len();
     println!("num signers: {}", num_signers);
 
-    let (pubkeys, public_nonces): (Vec<PublicKey>, Vec<PubNonce>) = sessions
-        .iter()
-        .map(|session| {
-            let resp = session.init_resp.clone();
-            let pk = PublicKey::from_str(resp.pubkey.as_str()).unwrap();
-            println!("pk: {}", pk);
-            let pn = PubNonce::from_hex(resp.pubnonce.as_str()).unwrap();
-            (pk, pn)
-        })
-        .collect();
+    let (pubkeys, public_nonces, key_agg_ctx, aggregated_pubkey, aggregated_nonce) =
+        aggregate_pubs(&sessions);
 
-    let key_agg_ctx = KeyAggContext::new(pubkeys.clone()).unwrap();
-    let aggregated_pubkey: Point = key_agg_ctx.aggregated_pubkey();
-    println!("agg pubkey X: {}", aggregated_pubkey);
-
-    let blinding_seed = rand::thread_rng().random::<[u8; 32]>();
-    let mut blinding_factors = vec![];
-    for i in 0..num_signers {
-        let blind_hash0: [u8; 32] = Sha256::new()
-            .chain_update(blinding_seed)
-            .chain_update(&(i as u32).to_be_bytes())
-            .chain_update(&(0 as u32).to_be_bytes())
-            .finalize()
-            .into();
-
-        let k0 = Scalar::from_slice(&blind_hash0).unwrap();
-
-        let blind_hash1: [u8; 32] = Sha256::new()
-            .chain_update(blinding_seed)
-            .chain_update(&(i as u32).to_be_bytes())
-            .chain_update(&(1 as u32).to_be_bytes())
-            .finalize()
-            .into();
-
-        let k1 = Scalar::from_slice(&blind_hash1).unwrap();
-        blinding_factors.push((k0, k1));
-    }
-
-    // We manually aggregate the nonces together and then construct our partial signature.
-    let aggregated_nonce: AggNonce = public_nonces.iter().sum();
+    let blinding_factors = gen_blinding_factors(num_signers);
 
     let aas: MaybeScalar = blinding_factors.iter().map(|(a, b)| *a).sum();
     let bbs: MaybePoint = blinding_factors
@@ -124,11 +78,154 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let nonce_x_bytes = adapted_nonce.serialize_xonly();
     let e: MaybeScalar =
         compute_challenge_hash_tweak(&nonce_x_bytes, &aggregated_pubkey.into(), &message);
-    let ee: Scalar = e.try_into().unwrap();
 
-    let pubkey: Point = pubkeys[0].into();
+    let key_parity = aggregated_pubkey.parity(); // ^ key_agg_ctx.parity_acc;
+    let nonce_parity = sign_nonce.parity();
 
-    let parity = aggregated_pubkey.parity(); // ^ key_agg_ctx.parity_acc;
+    let partial_signatures = request_partial_sigs(
+        sessions,
+        &key_agg_ctx,
+        aggregated_pubkey,
+        &blinding_factors,
+        sign_nonce,
+        e,
+    )
+        .await?;
+
+    verify_partial_sigs(
+        &public_nonces,
+        &key_agg_ctx,
+        aggregated_pubkey,
+        &blinding_factors,
+        sign_nonce,
+        e,
+        &partial_signatures,
+    );
+
+    let unblinded_sigs = unblind_partial_sigs(blinding_factors, sign_nonce, partial_signatures);
+
+    let final_signature = aggregate_partial_sigs(message, &key_agg_ctx, sign_nonce, unblinded_sigs);
+
+    musig2::verify_single(aggregated_pubkey, &final_signature, message)
+        .expect("aggregated signature must be valid");
+
+    Ok(())
+}
+
+struct SigningSession {
+    signer: String,
+    session_id: String,
+    init_resp: InitResp,
+}
+
+async fn init_signer_sessions(
+    cfg: &Config,
+) -> Result<Vec<SigningSession>, Box<dyn std::error::Error>> {
+    let mut sessions = vec![];
+
+    for s in &cfg.signers {
+        let id = hex::encode(rand::thread_rng().random::<[u8; 32]>());
+        let resp = reqwest::get(format!("http://{s}/init/{id}"))
+            .await?
+            .json::<InitResp>()
+            .await?;
+        println!("{resp:#?}");
+
+        let session = SigningSession {
+            signer: s.into(),
+            session_id: id.clone(),
+            init_resp: resp.clone(),
+        };
+
+        sessions.push(session);
+    }
+
+    Ok(sessions)
+}
+
+
+
+fn aggregate_partial_sigs(
+    message: &str,
+    key_agg_ctx: &KeyAggContext,
+    sign_nonce: MaybePoint,
+    unblinded_sigs: Vec<PartialSignature>,
+) -> [u8; 64] {
+    let final_signature: [u8; 64] = musig2::aggregate_partial_signatures_final_nonce(
+        &key_agg_ctx,
+        sign_nonce.try_into().unwrap(),
+        unblinded_sigs,
+        message,
+    )
+    .expect("error aggregating signatures");
+    final_signature
+}
+
+fn unblind_partial_sigs(
+    blinding_factors: Vec<(Scalar, Scalar)>,
+    sign_nonce: MaybePoint,
+    partial_signatures: Vec<MaybeScalar>,
+) -> Vec<PartialSignature> {
+    let unblinded_sigs: Vec<PartialSignature> = partial_signatures
+        .iter()
+        .enumerate()
+        .map(|(i, s)| {
+            if sign_nonce.has_even_y() {
+                *s + blinding_factors[i].0
+            } else {
+                *s - blinding_factors[i].0
+            }
+        })
+        .collect();
+    unblinded_sigs
+}
+
+fn verify_partial_sigs(
+    public_nonces: &Vec<PubNonce>,
+    key_agg_ctx: &KeyAggContext,
+    aggregated_pubkey: Point,
+    blinding_factors: &Vec<(Scalar, Scalar)>,
+    sign_nonce: MaybePoint,
+    e: MaybeScalar,
+    partial_signatures: &Vec<MaybeScalar>,
+) {
+    let key_parity = aggregated_pubkey.parity(); // ^ key_agg_ctx.parity_acc;
+    let nonce_parity = sign_nonce.parity();
+
+    /// Signatures should be verified upon receipt and invalid signatures
+    /// should be blamed on the signer who sent them.
+    for (i, partial_signature) in partial_signatures.clone().into_iter().enumerate() {
+        let their_pubkey: PublicKey = key_agg_ctx.get_pubkey(i).unwrap();
+        let their_pubnonce = &public_nonces[i];
+
+        let ep = if sign_nonce.has_even_y() ^ aggregated_pubkey.has_even_y() {
+            e - blinding_factors[i].1
+        } else {
+            e + blinding_factors[i].1
+        };
+
+        verify_partial_challenge(
+            key_agg_ctx.key_coefficient(their_pubkey).unwrap(),
+            key_parity,
+            partial_signature,
+            nonce_parity,
+            their_pubkey,
+            &their_pubnonce,
+            ep,
+        )
+        .expect("received invalid signature from a peer");
+    }
+}
+
+async fn request_partial_sigs(
+    sessions: Vec<SigningSession>,
+    key_agg_ctx: &KeyAggContext,
+    aggregated_pubkey: Point,
+    blinding_factors: &Vec<(Scalar, Scalar)>,
+    sign_nonce: MaybePoint,
+    e: MaybeScalar,
+) -> Result<(Vec<MaybeScalar>), Box<dyn std::error::Error>> {
+    let key_parity = aggregated_pubkey.parity(); // ^ key_agg_ctx.parity_acc;
     let nonce_parity = sign_nonce.parity();
 
     let mut partial_signatures = vec![];
@@ -150,7 +247,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         let body = SignReq {
             session_id: id.clone(),
-            key_parity: parity.unwrap_u8(),
+            key_parity: key_parity.unwrap_u8(),
             nonce_parity: nonce_parity.unwrap_u8(),
             key_coeff: key_coeff.encode_hex(),
             e: hex::encode(ep),
@@ -169,53 +266,66 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let p = PartialSignature::from_hex(&resp.sig).unwrap();
         partial_signatures.push(p);
     }
+    Ok(partial_signatures)
+}
 
-    /// Signatures should be verified upon receipt and invalid signatures
-    /// should be blamed on the signer who sent them.
-    for (i, partial_signature) in partial_signatures.clone().into_iter().enumerate() {
-        let their_pubkey: PublicKey = key_agg_ctx.get_pubkey(i).unwrap();
-        let their_pubnonce = &public_nonces[i];
+fn gen_blinding_factors(num_signers: usize) -> Vec<(Scalar, Scalar)> {
+    let blinding_seed = rand::thread_rng().random::<[u8; 32]>();
+    let mut blinding_factors = vec![];
+    for i in 0..num_signers {
+        let blind_hash0: [u8; 32] = Sha256::new()
+            .chain_update(blinding_seed)
+            .chain_update(&(i as u32).to_be_bytes())
+            .chain_update(&(0 as u32).to_be_bytes())
+            .finalize()
+            .into();
 
-        let ep = if sign_nonce.has_even_y() ^ aggregated_pubkey.has_even_y() {
-            e - blinding_factors[i].1
-        } else {
-            e + blinding_factors[i].1
-        };
+        let k0 = Scalar::from_slice(&blind_hash0).unwrap();
 
-        verify_partial_challenge(
-            key_agg_ctx.key_coefficient(their_pubkey).unwrap(),
-            parity,
-            partial_signature,
-            nonce_parity,
-            their_pubkey,
-            &their_pubnonce,
-            ep,
-        )
-        .expect("received invalid signature from a peer");
+        let blind_hash1: [u8; 32] = Sha256::new()
+            .chain_update(blinding_seed)
+            .chain_update(&(i as u32).to_be_bytes())
+            .chain_update(&(1 as u32).to_be_bytes())
+            .finalize()
+            .into();
+
+        let k1 = Scalar::from_slice(&blind_hash1).unwrap();
+        blinding_factors.push((k0, k1));
     }
+    blinding_factors
+}
 
-    let unblinded_sigs: Vec<PartialSignature> = partial_signatures
+fn aggregate_pubs(
+    sessions: &Vec<SigningSession>,
+) -> (
+    Vec<PublicKey>,
+    Vec<PubNonce>,
+    KeyAggContext,
+    Point,
+    AggNonce,
+) {
+    let (pubkeys, public_nonces): (Vec<PublicKey>, Vec<PubNonce>) = sessions
         .iter()
-        .enumerate()
-        .map(|(i, s)| {
-            if sign_nonce.has_even_y() {
-                *s + blinding_factors[i].0
-            } else {
-                *s - blinding_factors[i].0
-            }
+        .map(|session| {
+            let resp = session.init_resp.clone();
+            let pk = PublicKey::from_str(resp.pubkey.as_str()).unwrap();
+            println!("pk: {}", pk);
+            let pn = PubNonce::from_hex(resp.pubnonce.as_str()).unwrap();
+            (pk, pn)
         })
         .collect();
 
-    let final_signature: [u8; 64] = musig2::aggregate_partial_signatures_final_nonce(
-        &key_agg_ctx,
-        sign_nonce.try_into().unwrap(),
-        unblinded_sigs,
-        message,
+    let key_agg_ctx = KeyAggContext::new(pubkeys.clone()).unwrap();
+    let aggregated_pubkey: Point = key_agg_ctx.aggregated_pubkey();
+    println!("agg pubkey X: {}", aggregated_pubkey);
+
+    // We manually aggregate the nonces together and then construct our partial signature.
+    let aggregated_nonce: AggNonce = public_nonces.iter().sum();
+    (
+        pubkeys,
+        public_nonces,
+        key_agg_ctx,
+        aggregated_pubkey,
+        aggregated_nonce,
     )
-    .expect("error aggregating signatures");
-
-    musig2::verify_single(aggregated_pubkey, &final_signature, message)
-        .expect("aggregated signature must be valid");
-
-    Ok(())
 }
